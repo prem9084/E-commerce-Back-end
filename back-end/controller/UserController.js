2258312eabae83e862e2e918ac44d4b0e1e310d5
prem9084 <146803260+prem9084@users.noreply.github.com>
2024-01-07 18:55:43 +0530

    add new api

--- back-end/controller/UserController.js
+++ back-end/controller/UserController.js
@@ -13,3 +13,21 @@ export const getAllUsers = async (req, res) => {
     console.log(error);
   }
 };
+
+export const deleteUser = async (req, res) => {
+  try {
+    const user = await UserModel.findByIdAndDelete(req.params.id);
+    res.status(200).send({
+      success: true,
+      message: "User deleted successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).send({
+      success: false,
+      message: "Error while deleting user",
+      errro,
+    });
+  }
+};
--- back-end/routes/UserRoutes.js
+++ back-end/routes/UserRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllUsers } from "../controller/UserController.js";
+import { deleteUser, getAllUsers } from "../controller/UserController.js";
 
 const router = express.Router();
 
 router.get("/alluser", getAllUsers);
+router.delete("/delete/:id", deleteUser);
 
 export default router;
